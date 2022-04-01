95412b3b2df60814dab271ec3dba5b4f55529286
LucasLeRay <lucas.a.leray@gmail.com>
2022-04-01 09:26:16 +0200

    Update users pipeline wording

--- src/users/main.py
+++ src/users/main.py
@@ -1,7 +1,7 @@
 import logging
 
-from .fetch import fetch
-from .filter import filter
+from .fetch import fetch as fetch_users
+from .filter import filter as filter_users
 from .personality import add_personality
 
 logger = logging.getLogger(__name__)
@@ -9,14 +9,14 @@ logger = logging.getLogger(__name__)
 
 def main(count, *, lang, bio_only=False):
     logger.debug("Fetching users...")
-    users = fetch(count, lang=lang)
-    logger.debug(f"Fetched {len(users)} users...")
+    users = fetch_users(count, lang=lang)
+    logger.debug(f"Fetched {len(users)} users.")
 
     logger.debug("Adding personality columns to users...")
     users = add_personality(users, bio_only=bio_only)
 
-    logger.debug("Filtering users")
-    users = filter(users)
+    logger.debug("Filtering users...")
+    users = filter_users(users)
     logger.debug(f"Remaining users: {len(users)}")
 
     return users
