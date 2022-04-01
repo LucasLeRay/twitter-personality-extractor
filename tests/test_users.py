e8a74a46f8af9f03c270f0e2df8fa393885e4d95
LucasLeRay <lucas.a.leray@gmail.com>
2022-04-01 08:55:38 +0200

    Add tests to full user pipeline and fetch

---
+++ tests/test_users.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.users.main import main as users_pipeline
+
+TWEET_COUNT = 2
+LANG = "en"
+
+@pytest.mark.users
+class TestUsersPipeline:
+    @pytest.mark.parametrize("bio_only", [False, True])
+    def test_users_pipeline(self, bio_only):
+      users_pipeline(TWEET_COUNT, lang=LANG, bio_only=bio_only)
---
+++ tests/test_users_fetch.py
@@ -0,0 +1,57 @@
+import copy
+from types import SimpleNamespace
+from unittest.mock import patch
+
+import pytest
+
+from src.schemas import User
+from src.users import fetch
+
+FETCH_COUNT = 2
+FETCH_LANG = "en"
+
+
+@pytest.fixture
+def tweets():
+    tweet = SimpleNamespace(
+      id="123abc",
+      text="I am ENTJ omg :o",
+      user=SimpleNamespace(
+        id="456def",
+        description="I am Lucas ;)",
+        statuses_count=123,
+        followers_count=456,
+        friends_count=789,
+        listed_count=0
+      )
+    )
+    tweet2 = copy.deepcopy(tweet)
+    tweet2.user.id = "789ghi"
+    return [tweet, tweet2]
+
+
+@pytest.mark.users
+class TestUserFetch:
+    def test_format_user(self, tweets):
+        expected_keys = set(list(User)) - set([
+            User.EXTRAVERSION,
+            User.INTUITION,
+            User.THINKING,
+            User.JUDGING,
+            User.ID
+        ])
+
+        formatted = fetch._format_user(tweets[0])
+
+        assert set(formatted.keys()) == expected_keys
+        assert None not in formatted.values()
+
+    @patch("src.users.fetch._search_cursor")
+    def test_fetch_user(self, _search_cursor, tweets):
+        _search_cursor.return_value = tweets
+        expected_cols = set(list(User)) - set([User.ID])
+
+        users = fetch.fetch(FETCH_COUNT, lang=FETCH_LANG)
+
+        assert len(users) == FETCH_COUNT
+        assert set(users.columns) == expected_cols
