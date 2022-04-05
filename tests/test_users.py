e1a9d111e5da1aadedf72f161d897672e5e3ed82
LucasLeRay <lucas.a.leray@gmail.com>
2022-04-05 11:33:01 +0200

    Update indentation of test_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,4 +9,4 @@ LANG = "en"
 class TestUsersPipeline:
     @pytest.mark.parametrize("bio_only", [False, True])
     def test_users_pipeline(self, bio_only):
-      users_pipeline(TWEET_COUNT, lang=LANG, bio_only=bio_only)
+        users_pipeline(TWEET_COUNT, lang=LANG, bio_only=bio_only)
