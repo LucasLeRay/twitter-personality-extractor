915463a7f9caa73f851667e5eed4e2d02fc93654
LucasLeRay <lucas.a.leray@gmail.com>
2022-04-06 09:50:11 +0200

    Add test workflow

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv, find_dotenv
 LOGGING_FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
 
 logging.basicConfig(format=LOGGING_FORMAT)
+logger = logging.getLogger()
 
 try:
     load_dotenv(find_dotenv(raise_error_if_not_found=True))
 except OSError:
-    raise RuntimeError("'.env' file not found.")
+    logger.warn("'.env' file not found.")
